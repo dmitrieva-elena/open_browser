e2e0f9ce3f2bc372e597d3eef2888f1cc983b5a5
dmitrieva-elena <s126511ssv@gmail.com>
2021-10-28 11:49:41 +0700

    add script for google.com

--- open_browser.py
+++ open_browser.py
@@ -1,11 +1,37 @@
 import time
 from selenium import webdriver
+
+
+def OpenLitres(driver):
+    '''
+        Open https://www.litres.ru/ in Google Chrome
+        and search Isaac Asimov's books
+
+    '''
+    driver.get("https://www.litres.ru/")
+    textinput = driver.find_element_by_css_selector("[class=\"Search-module__input\"]")
+    textinput.send_keys("Айзек Азимов")
+    submit_button = driver.find_element_by_css_selector("[class=\"Search-module__button\"]")
+    submit_button.click()
+    time.sleep(5)
+    driver.quit()
+
+
+def OpenGoogle(driver):
+    '''
+        Open https://www.google.com/ in Google Chrome
+        and search info about Isaac Asimov
+        
+    '''
+    driver.get("https://www.google.com/")
+    textinput = search = driver.find_element_by_css_selector("[class=\"gLFyf gsfi\"]")
+    textinput.send_keys("Айзек Азимов")
+    submit_button = driver.find_elements_by_css_selector("[class=\"gNO89b\"]")
+    submit_button[-1].click()
+    time.sleep(5)
+    driver.quit()
+
+
 driver = webdriver.Chrome()
 time.sleep(5)
-driver.get("https://www.litres.ru/")
-textinput = driver.find_element_by_css_selector("[class=\"Search-module__input\"]")
-textinput.send_keys("Айзек Азимов")
-submit_button = driver.find_element_by_css_selector("[class=\"Search-module__button\"]")
-submit_button.click()
-time.sleep(5)
-driver.quit()
+OpenGoogle(driver)
